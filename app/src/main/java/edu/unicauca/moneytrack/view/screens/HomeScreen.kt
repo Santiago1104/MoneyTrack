389ffcc75853f39d228f3318a725dc4f59daf14b
Santiago1104 <dasamunoz@unicauca.edu.co>
2024-09-28 09:56:59 -0500

    ADD: Strings en XML y avance Screen Home

--- app/src/main/java/edu/unicauca/moneytrack/view/screens/HomeScreen.kt
+++ app/src/main/java/edu/unicauca/moneytrack/view/screens/HomeScreen.kt
@@ -1,6 +1,7 @@
 package edu.unicauca.moneytrack.view.screens
 
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -12,6 +13,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -23,8 +25,9 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.res.stringResource
+import edu.unicauca.moneytrack.R
 import edu.unicauca.moneytrack.viewmodel.MoneyViewModel
-
 
 @Composable
 fun HomeScreen(
@@ -36,7 +39,6 @@ fun HomeScreen(
     val ingresos by viewModel.listaIngresos.observeAsState(emptyList())
     val gastos by viewModel.listaGastos.observeAsState(emptyList())
 
-    // Calculamos el total de ingresos y gastos
     val totalIngresos = ingresos.sumOf { it.valor }
     val totalGastos = gastos.sumOf { it.valor }
 
@@ -46,11 +48,10 @@ fun HomeScreen(
             .padding(16.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(text = "Money Track", style = MaterialTheme.typography.headlineMedium )
+        Text(text = stringResource(id = R.string.AppTitle), style = MaterialTheme.typography.headlineMedium )
 
         Spacer(modifier = Modifier.height(16.dp))
 
-        // Componente de la barra circular
         CircularProgressIndicatorWithText(
             ingresos = totalIngresos,
             gastos = totalGastos,
@@ -59,22 +60,28 @@ fun HomeScreen(
 
         Spacer(modifier = Modifier.height(32.dp))
 
-        // Botones de Nuevo Gasto e Ingreso
         Row(
             horizontalArrangement = Arrangement.SpaceEvenly,
             modifier = Modifier.fillMaxWidth()
         ) {
-            Button(onClick = onAddGastoClick) {
-                Text("Nuevo gasto")
+
+            Button(
+                onClick = onAddIngresoClick,
+                colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF2A65D8))
+            ) {
+                Text(stringResource(id = R.string.AddEntryButton), color = Color.White)
             }
-            Button(onClick = onAddIngresoClick) {
-                Text("Nuevo ingreso")
+            Button(
+                onClick = onAddGastoClick,
+                colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF39C9CD))
+            ) {
+                Text(stringResource(id = R.string.AddExpenseButton), color = Color.White)
             }
         }
 
         Spacer(modifier = Modifier.height(16.dp))
 
-        Text(text = "Recientes", style = MaterialTheme.typography.headlineMedium )
+        Text(text = stringResource(id = R.string.Recents), style = MaterialTheme.typography.headlineSmall, modifier = Modifier.align(Alignment.Start) )
     }
 }
 
@@ -92,7 +99,6 @@ fun CircularProgressIndicatorWithText(
         Canvas(modifier = Modifier.size(200.dp)) {
             val strokeWidth = 16.dp.toPx()
 
-            // Fondo de la barra circular
             drawArc(
                 color = Color.LightGray,
                 startAngle = 0f,
@@ -101,18 +107,16 @@ fun CircularProgressIndicatorWithText(
                 style = Stroke(strokeWidth, cap = StrokeCap.Round)
             )
 
-            // Barra de ingresos
             drawArc(
-                color = Color(0xFF03DAC5),
+                color = Color(0xFF2A65D8),
                 startAngle = 270f,
                 sweepAngle = (progressIngresos * 360).toFloat(),
                 useCenter = false,
                 style = Stroke(strokeWidth, cap = StrokeCap.Round)
             )
 
-            // Barra de gastos
             drawArc(
-                color = Color(0xFF6200EA),
+                color = Color(0xFF39C9CD),
                 startAngle = 270f + (progressIngresos * 360).toFloat(),
                 sweepAngle = (progressGastos * 360).toFloat(),
                 useCenter = false,
@@ -120,12 +124,33 @@ fun CircularProgressIndicatorWithText(
             )
         }
 
-        // Texto del dinero actual
         Column(horizontalAlignment = Alignment.CenterHorizontally) {
             Text(text = "$${dineroActual.format(2)}", style = MaterialTheme.typography.headlineMedium )
-            Text(text = "Dinero actual", style = MaterialTheme.typography.bodyMedium)
+            Text(text = stringResource(id = R.string.CurrentMoney), style = MaterialTheme.typography.bodyMedium)
         }
     }
+
+    Spacer(modifier = Modifier.height(16.dp))
+
+    Row(
+        horizontalArrangement = Arrangement.SpaceEvenly,
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Box(modifier = Modifier.size(16.dp).padding(end = 4.dp).background(Color(0xFF2A65D8)))
+            Text(text = stringResource(id = R.string.Entrys), style = MaterialTheme.typography.bodyMedium)
+        }
+
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Box(modifier = Modifier.size(16.dp).padding(end = 4.dp).background(Color(0xFF39C9CD)))
+            Text(text = stringResource(id = R.string.Expenses), style = MaterialTheme.typography.bodyMedium)
+        }
+    }
+
 }
 
 fun Double.format(digits: Int) = "%.${digits}f".format(this)
